4e3508ccbd046cc56d8acdd5697cb04dde77c8ca
nanipi <info@libracore.com>
2021-07-28 14:08:55 +0200

    separated kuendigung from mietvertrag

---
+++ sollbruchstelle/sollbruchstelle/doctype/kuendigung/kuendigung.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2021, Libracore AG and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Kuendigung', {
+	// refresh: function(frm) {
+
+	// }
+});
